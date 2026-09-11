f9a950f2162777b745f3940ec1d41518ee32656c
mcangeli <mcangeli@gmail.com>
2026-09-11 13:19:10 -0400

    Add protected course document route

--- portal/course_urls.py
+++ portal/course_urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("shows/<int:show_pk>/courses/add/", course_views.show_course_add, name="show_course_add"),
     path("shows/<int:show_pk>/courses/<int:course_pk>/edit/", course_views.show_course_edit, name="show_course_edit"),
     path("shows/<int:show_pk>/courses/<int:course_pk>/media/", course_views.show_course_media, name="show_course_media"),
+    path("shows/<int:show_pk>/courses/<int:course_pk>/document/", course_views.show_course_document, name="show_course_document"),
     path("shows/<int:show_pk>/courses/<int:course_pk>/remove/", course_views.show_course_remove, name="show_course_remove"),
 ]
