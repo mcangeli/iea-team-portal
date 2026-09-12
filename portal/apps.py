3db9869a355277d6407ee22f5279aac3b1006102
mcangeli <mcangeli@gmail.com>
2026-09-12 13:00:16 -0400

    refactor: separate model loading from signal behavior

--- portal/apps.py
+++ portal/apps.py
@@ -11,7 +11,6 @@ class PortalConfig(AppConfig):
         from . import branding_models  # noqa: F401
         from . import horse_models  # noqa: F401
         from . import hoofprint_models  # noqa: F401
-        from . import season_class_code  # noqa: F401
         from . import show_readiness_models  # noqa: F401
         from . import course_models  # noqa: F401
         from . import post_show_horse_models  # noqa: F401
