02f18ac645106d5c61eec3fbf44cf9908d6e7d45
mcangeli <mcangeli@gmail.com>
2026-09-13 22:57:12 -0400

    feat: add grouped public show schedule

--- portal/view_modules/public_site.py
+++ portal/view_modules/public_site.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 from portal.publication import (
     get_public_show,
@@ -9,15 +10,39 @@ from portal.publication import (
 )
 
 
+def _public_show_groups(site):
+    today = timezone.localdate()
+    shows = [public_show_payload(publication) for publication in public_show_queryset(site)]
+    upcoming = [show for show in shows if show["date"] >= today]
+    past = [show for show in shows if show["date"] < today]
+    past.reverse()
+    return upcoming, past
+
+
 def public_site_home(request, site_slug):
     site = get_public_site(site_slug)
-    shows = [public_show_payload(publication) for publication in public_show_queryset(site)]
+    upcoming, _past = _public_show_groups(site)
     return render(
         request,
         "public/site_home.html",
         {
             "public_site": public_site_payload(site),
-            "public_shows": shows,
+            "public_shows": upcoming[:3],
+            "has_more_shows": len(upcoming) > 3,
+        },
+    )
+
+
+def public_schedule(request, site_slug):
+    site = get_public_site(site_slug)
+    upcoming, past = _public_show_groups(site)
+    return render(
+        request,
+        "public/schedule.html",
+        {
+            "public_site": public_site_payload(site),
+            "upcoming_shows": upcoming,
+            "past_shows": past,
         },
     )
 
