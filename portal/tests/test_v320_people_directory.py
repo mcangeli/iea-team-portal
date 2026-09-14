f84ee9be944e9f879eba7bb0f1560744cefe5d9a
mcangeli <mcangeli@gmail.com>
2026-09-14 17:31:38 -0400

    test: reuse auto-created user profiles in v3.2 directory tests

--- portal/tests/test_v320_people_directory.py
+++ portal/tests/test_v320_people_directory.py
@@ -15,17 +15,17 @@ class V320PeopleDirectoryTests(TestCase):
         self.admin = User.objects.create_user(
             username="admin-v320", password="pass12345", first_name="Admin", last_name="User"
         )
-        UserProfile.objects.create(user=self.admin, team=self.team, role=UserProfile.Role.ADMIN)
+        self._configure_profile(self.admin, self.team, UserProfile.Role.ADMIN)
 
         self.member = User.objects.create_user(
             username="member-v320", password="pass12345", first_name="Jamie", last_name="Rider"
         )
-        UserProfile.objects.create(user=self.member, team=self.team, role=UserProfile.Role.RIDER)
+        self._configure_profile(self.member, self.team, UserProfile.Role.RIDER)
 
         self.other_user = User.objects.create_user(
             username="other-v320", password="pass12345", first_name="Other", last_name="User"
         )
-        UserProfile.objects.create(user=self.other_user, team=self.other_team, role=UserProfile.Role.ADMIN)
+        self._configure_profile(self.other_user, self.other_team, UserProfile.Role.ADMIN)
 
         self.person = Person.objects.create(
             team=self.team,
@@ -42,6 +42,14 @@ class V320PeopleDirectoryTests(TestCase):
             person=self.person,
             role=OrganizationRoleAssignment.Role.RIDER,
         )
+
+    @staticmethod
+    def _configure_profile(user, team, role):
+        profile = user.profile
+        profile.team = team
+        profile.role = role
+        profile.save(update_fields=["team", "role"])
+        return profile
 
     def test_authenticated_member_can_view_people_directory(self):
         self.client.force_login(self.member)
@@ -60,7 +68,7 @@ class V320PeopleDirectoryTests(TestCase):
 
     def test_other_non_manager_does_not_receive_private_fields(self):
         peer_user = User.objects.create_user(username="peer-v320", password="pass12345")
-        UserProfile.objects.create(user=peer_user, team=self.team, role=UserProfile.Role.PARENT)
+        self._configure_profile(peer_user, self.team, UserProfile.Role.PARENT)
         Person.objects.create(team=self.team, user=peer_user, first_name="Peer", last_name="Parent")
         self.client.force_login(peer_user)
         response = self.client.get(reverse("person_detail", args=[self.person.pk]))
@@ -76,7 +84,7 @@ class V320PeopleDirectoryTests(TestCase):
 
     def test_manager_can_create_person_and_attach_same_team_django_account(self):
         unused_user = User.objects.create_user(username="new-person-v320", password="pass12345")
-        UserProfile.objects.create(user=unused_user, team=self.team, role=UserProfile.Role.PARENT)
+        self._configure_profile(unused_user, self.team, UserProfile.Role.PARENT)
         self.client.force_login(self.admin)
         response = self.client.post(
             reverse("person_create"),
