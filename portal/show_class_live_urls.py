52a157ed098228e653fae4f76befbdc8470238fa
mcangeli <mcangeli@gmail.com>
2026-09-14 13:11:53 -0400

    feat: route class result publication action

--- portal/show_class_live_urls.py
+++ portal/show_class_live_urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from portal.view_modules.show_class_live import show_class_live_status_update
+from portal.view_modules.show_class_live import (
+    show_class_live_status_update,
+    show_class_results_publication_update,
+)
 
 urlpatterns = [
     path(
         "show-classes/<int:class_pk>/live-status/",
         show_class_live_status_update,
         name="show_class_live_status_update",
+    ),
+    path(
+        "show-classes/<int:class_pk>/results-publication/",
+        show_class_results_publication_update,
+        name="show_class_results_publication_update",
     ),
 ]
