9a9bf8d4990591b09a7b6d6b80d623b774340bb8
mcangeli <mcangeli@gmail.com>
2026-09-13 22:57:20 -0400

    feat: add public schedule route

--- portal/public_urls.py
+++ portal/public_urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from portal.view_modules.public_site import public_show_detail, public_site_home
+from portal.view_modules.public_site import public_schedule, public_show_detail, public_site_home
 
 
 urlpatterns = [
     path("public/<slug:site_slug>/", public_site_home, name="public_site_home"),
+    path(
+        "public/<slug:site_slug>/schedule/",
+        public_schedule,
+        name="public_schedule",
+    ),
     path(
         "public/<slug:site_slug>/shows/<slug:show_slug>/",
         public_show_detail,
