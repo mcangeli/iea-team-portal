b900d92f4fae0eaea7455b0a57604eb17a664ed9
mcangeli <mcangeli@gmail.com>
2026-09-14 14:18:13 -0400

    test: protect stable public live board URL

---
+++ portal/tests/test_v310_public_live_board_url.py
@@ -0,0 +1,73 @@
+from datetime import date
+
+from django.test import TestCase
+from django.urls import reverse
+
+from portal.model_modules.public_site import PublicShowPublication, PublicSiteProfile
+from portal.models import Season, Show, Team
+
+
+class PublicLiveBoardUrlTests(TestCase):
+    def setUp(self):
+        self.team = Team.objects.create(name="Blue Skies Riding Academy")
+        self.season = Season.objects.create(
+            team=self.team,
+            name="2026-2027",
+            start_date=date(2026, 8, 1),
+            end_date=date(2027, 6, 30),
+            is_active=True,
+        )
+        self.site = PublicSiteProfile.objects.create(
+            team=self.team,
+            slug="blue-skies-riding-academy",
+            enabled=True,
+        )
+        self.show = Show.objects.create(
+            team=self.team,
+            season=self.season,
+            name="Autumn Invitational",
+            show_date=date(2026, 10, 10),
+            status=Show.Status.IN_PROGRESS,
+        )
+        self.publication = PublicShowPublication.objects.create(
+            show=self.show,
+            slug="autumn-invitational-live",
+            is_published=True,
+            publish_live_status=True,
+        )
+
+    def test_live_url_redirects_to_active_public_show(self):
+        response = self.client.get(reverse("public_live_board", args=[self.site.slug]))
+        self.assertRedirects(
+            response,
+            reverse("public_show_detail", args=[self.site.slug, self.publication.slug]),
+            fetch_redirect_response=False,
+        )
+
+    def test_live_url_falls_back_to_schedule_when_nothing_is_live(self):
+        self.show.status = Show.Status.ENTERED
+        self.show.save(update_fields=["status"])
+
+        response = self.client.get(reverse("public_live_board", args=[self.site.slug]))
+        self.assertRedirects(
+            response,
+            reverse("public_schedule", args=[self.site.slug]),
+            fetch_redirect_response=False,
+        )
+
+    def test_unpublished_live_show_is_not_exposed_by_live_url(self):
+        self.publication.is_published = False
+        self.publication.save(update_fields=["is_published"])
+
+        response = self.client.get(reverse("public_live_board", args=[self.site.slug]))
+        self.assertRedirects(
+            response,
+            reverse("public_schedule", args=[self.site.slug]),
+            fetch_redirect_response=False,
+        )
+
+    def test_public_navigation_exposes_live_link(self):
+        response = self.client.get(reverse("public_site_home", args=[self.site.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, reverse("public_live_board", args=[self.site.slug]))
+        self.assertContains(response, ">Live<")
