01c3b19356cead06ca50325f09ad7def8d817762
mcangeli <mcangeli@gmail.com>
2026-09-14 17:13:05 -0400

    feat: allow safe Django account linkage on Person profiles

--- portal/people_forms.py
+++ portal/people_forms.py
@@ -1,12 +1,29 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from portal.model_modules.people import Person
 
 
 class PersonForm(forms.ModelForm):
+    def __init__(self, *args, team=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        user_field = self.fields["user"]
+        if team is None:
+            user_field.queryset = User.objects.none()
+        else:
+            user_field.queryset = (
+                User.objects.filter(profile__team=team)
+                .order_by("last_name", "first_name", "username")
+            )
+        user_field.required = False
+        user_field.help_text = (
+            "Optional Django login for this person. One login can be connected to only one ArenaLine Person."
+        )
+
     class Meta:
         model = Person
         fields = [
+            "user",
             "first_name",
             "last_name",
             "preferred_name",
