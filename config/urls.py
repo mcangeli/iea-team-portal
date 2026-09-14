16d97004e03d9edfc1f5ff5cb0d783d4e95d90bd
mcangeli <mcangeli@gmail.com>
2026-09-13 22:12:59 -0400

    feat: mount public publication routes

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("portal.public_urls")),
     path("", include("portal.branding_urls")),
     path("", include("portal.account_urls")),
     path("", include("portal.lifecycle_urls")),
