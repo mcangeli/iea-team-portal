11697d928a07957d7ef200cdc02d5ff3a26690fa
mcangeli <mcangeli@gmail.com>
2026-09-14 11:32:47 -0400

    feat: render published results on public show detail

--- portal/view_modules/public_site.py
+++ portal/view_modules/public_site.py
@@ -6,6 +6,7 @@ from portal.publication import (
     get_public_site,
     public_show_payload,
     public_show_queryset,
+    public_show_results_payload,
     public_show_schedule_payload,
     public_site_payload,
 )
@@ -58,5 +59,6 @@ def public_show_detail(request, site_slug, show_slug):
             "public_site": public_site_payload(site),
             "public_show": public_show_payload(publication),
             "public_class_schedule": public_show_schedule_payload(publication),
+            "public_results": public_show_results_payload(publication),
         },
     )
