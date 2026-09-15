e47329af43e9a972107e5ebfaf8893953a5fca6b
mcangeli <mcangeli@gmail.com>
2026-09-15 18:49:04 -0400

    Fix horses auth decorator import

--- portal/view_modules/horses.py
+++ portal/view_modules/horses.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.decorators.http import require_POST
