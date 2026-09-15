a04dd28b823f0d68f79a748339f5f3371b81da24
mcangeli <mcangeli@gmail.com>
2026-09-15 18:58:05 -0400

    Register v3.3 equine care models

--- portal/apps.py
+++ portal/apps.py
@@ -11,6 +11,7 @@ class PortalConfig(AppConfig):
         from .model_modules import branding  # noqa: F401
         from .model_modules import horses  # noqa: F401
         from .model_modules import equine_registry  # noqa: F401
+        from .model_modules import equine_care  # noqa: F401
         from .model_modules import hoofprint  # noqa: F401
         from .model_modules import show_readiness  # noqa: F401
         from .model_modules import competition_iea  # noqa: F401
