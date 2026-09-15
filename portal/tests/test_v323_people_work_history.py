0f6b3045912a8b76930bc78a5338aa1ab5a889cf
mcangeli <mcangeli@gmail.com>
2026-09-15 10:43:56 -0400

    test: lock work history privacy boundary

--- portal/tests/test_v323_people_work_history.py
+++ portal/tests/test_v323_people_work_history.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from portal.model_modules.people import Person
+from portal.model_modules.people import Person, PersonRelationship
 from portal.model_modules.station import WorkShiftEntry
 from portal.models import Team, UserProfile
 
@@ -50,6 +50,43 @@ class V323PeopleWorkHistoryTests(TestCase):
         self.assertEqual(response.context["work_summary"]["completed_count"], 2)
         self.assertEqual(response.context["work_summary"]["pending_count"], 1)
         self.assertEqual(response.context["work_summary"]["open_count"], 1)
+
+    def test_person_can_view_own_work_history(self):
+        worker = User.objects.create_user(username="worker-self", password="pass12345")
+        profile = worker.profile
+        profile.team = self.team
+        profile.role = UserProfile.Role.RIDER
+        profile.save(update_fields=["team", "role"])
+        self.person.user = worker
+        self.person.save(update_fields=["user", "updated_at"])
+        self._shift(minutes=60, approved=True)
+        self.client.force_login(worker)
+        response = self.client.get(reverse("person_detail", args=[self.person.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Work history")
+        self.assertEqual(response.context["work_summary"]["completed_count"], 1)
+
+    def test_parent_private_profile_access_does_not_expose_work_history(self):
+        parent_user = User.objects.create_user(username="work-parent", password="pass12345")
+        profile = parent_user.profile
+        profile.team = self.team
+        profile.role = UserProfile.Role.PARENT
+        profile.save(update_fields=["team", "role"])
+        parent = Person.objects.create(team=self.team, user=parent_user, first_name="Morgan", last_name="Parent")
+        PersonRelationship.objects.create(
+            from_person=parent,
+            to_person=self.person,
+            relationship_type=PersonRelationship.RelationshipType.PARENT_GUARDIAN,
+            active=True,
+        )
+        self._shift(minutes=60, approved=True)
+        self.client.force_login(parent_user)
+        response = self.client.get(reverse("person_detail", args=[self.person.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context["can_view_private"])
+        self.assertFalse(response.context["can_view_work_history"])
+        self.assertNotContains(response, "Work history")
+        self.assertIsNone(response.context["work_summary"])
 
     def test_work_history_is_not_exposed_without_private_person_access(self):
         self._shift(minutes=60, approved=True)
