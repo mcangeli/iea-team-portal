4d2618b283063937137bb957a26ec4ee6929dbc9
mcangeli <mcangeli@gmail.com>
2026-09-16 10:05:43 -0400

    v3.4.0 Preview 3: add lesson program and series forms

---
+++ portal/forms_lessons_v340.py
@@ -0,0 +1,64 @@
+from django import forms
+
+from .model_modules.lessons import LessonProgram, LessonSeries
+from .model_modules.people import OrganizationGroup, Person
+
+
+class LessonProgramForm(forms.ModelForm):
+    class Meta:
+        model = LessonProgram
+        fields = ["name", "group", "description", "default_capacity", "enrollment_opens", "enrollment_closes", "active"]
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 4}),
+            "enrollment_opens": forms.DateInput(attrs={"type": "date"}),
+            "enrollment_closes": forms.DateInput(attrs={"type": "date"}),
+        }
+
+    def __init__(self, *args, team, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.team = team
+        self.instance.team = team
+        self.fields["group"].queryset = OrganizationGroup.objects.filter(
+            team=team, group_type=OrganizationGroup.GroupType.PROGRAM, active=True
+        ).order_by("name")
+
+    def save(self, commit=True):
+        obj = super().save(commit=False)
+        obj.team = self.team
+        if commit:
+            obj.full_clean()
+            obj.save()
+        return obj
+
+
+class LessonSeriesForm(forms.ModelForm):
+    class Meta:
+        model = LessonSeries
+        fields = [
+            "name", "instructor", "weekday", "starts_at_time", "duration_minutes",
+            "default_location", "capacity", "start_date", "end_date", "active", "notes",
+        ]
+        widgets = {
+            "starts_at_time": forms.TimeInput(attrs={"type": "time"}),
+            "start_date": forms.DateInput(attrs={"type": "date"}),
+            "end_date": forms.DateInput(attrs={"type": "date"}),
+            "notes": forms.Textarea(attrs={"rows": 3}),
+        }
+
+    def __init__(self, *args, program, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.program = program
+        self.instance.program = program
+        self.fields["instructor"].queryset = Person.objects.filter(team=program.team, active=True).order_by("last_name", "first_name")
+        self.fields["weekday"].widget = forms.Select(choices=[
+            (0, "Monday"), (1, "Tuesday"), (2, "Wednesday"), (3, "Thursday"),
+            (4, "Friday"), (5, "Saturday"), (6, "Sunday"),
+        ])
+
+    def save(self, commit=True):
+        obj = super().save(commit=False)
+        obj.program = self.program
+        if commit:
+            obj.full_clean()
+            obj.save()
+        return obj
