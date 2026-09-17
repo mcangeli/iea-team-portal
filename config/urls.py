99b7b8635f5dfed265b60b5fce5e8d4159e6077c
mcangeli <mcangeli@gmail.com>
2026-09-17 17:53:00 -0400

    Wire v3.5 finance workspace routes

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("", include("portal.people_urls")),
     path("", include("portal.station_urls")),
     path("", include("portal.lesson_program_urls")),
+    path("", include("portal.finance_v350_urls")),
     path("", include("portal.urls")),
 ]
 
