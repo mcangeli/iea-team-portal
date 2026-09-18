f691826e0d41a14dcb821c41f6f669e55fd2f3e0
mcangeli <mcangeli@gmail.com>
2026-09-18 14:50:59 -0400

    Route ArenaLine login through optional MFA

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from portal.auth_views import ArenaLineLoginView, mfa_login_challenge
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", ArenaLineLoginView.as_view(), name="login"),
+    path("accounts/mfa/", mfa_login_challenge, name="mfa_login_challenge"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("portal.public_urls")),
     path("", include("portal.public_site_admin_urls")),
