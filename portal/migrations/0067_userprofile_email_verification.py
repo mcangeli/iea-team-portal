b06c394d1cb8a90369f9d9e33cbf2d30a9469872
mcangeli <mcangeli@gmail.com>
2026-09-18 13:59:26 -0400

    Migrate existing accounts into verified email state

---
+++ portal/migrations/0067_userprofile_email_verification.py
@@ -0,0 +1,33 @@
+from django.db import migrations, models
+
+
+def trust_existing_emails(apps, schema_editor):
+    UserProfile = apps.get_model("portal", "UserProfile")
+    UserProfile.objects.filter(
+        user__email__isnull=False,
+    ).exclude(user__email="").update(email_verified_at=models.functions.Now())
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("portal", "0066_showeventfinancialsettings_showfinancialeventmapping_and_more"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="userprofile",
+            name="email_verified_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name="userprofile",
+            name="pending_email",
+            field=models.EmailField(blank=True, max_length=254),
+        ),
+        migrations.AddField(
+            model_name="userprofile",
+            name="pending_email_requested_at",
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.RunPython(trust_existing_emails, migrations.RunPython.noop),
+    ]
