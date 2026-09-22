b037f0d8038c51189f1f40cf890bf104c76940c9
mcangeli <mcangeli@gmail.com>
2026-09-22 19:09:02 -0400

    Register v3.8.4 inventory models

--- portal/apps.py
+++ portal/apps.py
@@ -26,6 +26,7 @@ class PortalConfig(AppConfig):
         from .model_modules import lessons  # noqa: F401
         from .model_modules import finance  # noqa: F401
         from .model_modules import facilities  # noqa: F401
+        from .model_modules import inventory  # noqa: F401
         from . import course_models  # noqa: F401
         from .model_modules import post_show_horses  # noqa: F401
         from . import host_show_models  # noqa: F401
