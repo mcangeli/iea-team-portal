008be62f974ed750acf3a63f842eb2a7f7e64aa1
mcangeli <mcangeli@gmail.com>
2026-09-24 12:19:57 -0400

    fix: import timezone for canonical family unlink

--- portal/view_modules/roster_v323.py
+++ portal/view_modules/roster_v323.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.db.models import Prefetch, Q, Sum
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
 from django.views.decorators.http import require_POST
 
 from portal.forms import GuardianContactForm
