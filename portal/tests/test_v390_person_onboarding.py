1eae14289606abf4b5907e02a0b8b62bc4df88a1
mcangeli <mcangeli@gmail.com>
2026-09-24 11:15:51 -0400

    test: prove legacy Rider login URL is a Person adapter

--- portal/tests/test_v390_person_onboarding.py
+++ portal/tests/test_v390_person_onboarding.py
@@ -25,6 +25,15 @@ class V390PersonOnboardingTests(TestCase):
         self.admin.profile.save(update_fields=["team", "role"])
         self.client.force_login(self.admin)
 
+    def test_legacy_rider_login_create_route_redirects_to_person_access(self):
+        rider = Rider.objects.create(team=self.team, first_name="Legacy", last_name="Login")
+        person = Person.objects.create(team=self.team, first_name="Legacy", last_name="Login")
+        LegacyPersonLink.objects.create(person=person, rider=rider)
+
+        response = self.client.get(reverse("user_create_rider", args=[rider.pk]))
+
+        self.assertRedirects(response, reverse("person_login_create", args=[person.pk]))
+
     def test_legacy_rider_edit_route_redirects_to_person(self):
         rider = Rider.objects.create(team=self.team, first_name="Legacy", last_name="Edit")
         person = Person.objects.create(team=self.team, first_name="Legacy", last_name="Edit")
